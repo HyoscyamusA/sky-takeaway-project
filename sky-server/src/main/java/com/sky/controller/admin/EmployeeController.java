2ff1fccd680d455439e21bc838476e67e278d2e3
Ra9narok <2644406839@qq.com>
2025-07-21 14:53:38 +0800

    启用禁用员工功能

--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -107,4 +107,19 @@ public class EmployeeController {
         PageResult pageResult=employeeService.pageQuery(employeePageQueryDTO);
         return Result.success(pageResult);
     }
+
+    /**
+     * 启用禁用员工账号
+     * @param status
+     * @param id
+     * @return
+     */
+    @PostMapping("/status/{status}")
+    @ApiOperation("启用禁用员工账号")
+    //由于一致，故@PathVariable("status")中可以省略掉()内容
+    public Result startOrStop(@PathVariable Integer status,Long id) {
+        log.info("启用禁用员工账号:{},{}",status,id);
+        employeeService.startOrStop(status,id);
+        return Result.success();
+    }
 }
