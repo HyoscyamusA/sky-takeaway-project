4c69383986e4b5932ca3a5008c4e3035a79ad969
Ra9narok <2644406839@qq.com>
2025-08-03 00:56:13 +0800

    新增菜品代码部分

--- sky-server/src/main/java/com/sky/controller/admin/CommonController.java
+++ sky-server/src/main/java/com/sky/controller/admin/CommonController.java
@@ -1,6 +1,7 @@
 package com.sky.controller.admin;
 
 
+import com.sky.constant.MessageConstant;
 import com.sky.result.Result;
 import com.sky.utils.AliOssUtil;
 import io.swagger.annotations.Api;
@@ -49,12 +50,12 @@ public class CommonController {
             String filePath=aliOssUtil.upload(file.getBytes(), objectName);
             return Result.success(filePath);
 
-            
+
         } catch (IOException e) {
             log.error("文件上传失败:{}",e);
             e.printStackTrace();
         }
-        return null;
+        return Result.error(MessageConstant.UPLOAD_FAILED);
     }
 
 }
